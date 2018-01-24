908c1ee55217a316166ea020d7375191ed71bf27
mmanoeljorn <mmanoel@sjcc.com.br>
2018-01-24 11:19:40 -0300

    add project with laravel

--- process_ieee.php
+++ process_ieee.php
@@ -1,4 +1,8 @@
 <?php
+
+    require_once("class/config.class.php");
+
+    var_dump(ConnectionRDB::getInstance()); exit;
     $documents = file("ieeee_export2018.01.23-18.47.04.csv");
     foreach($documents as $document) {
         $data = explode("\",\"", $document);
